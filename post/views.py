c83bda4778367939abf2aff6d93886c709541d06
ALKEMIA-CHARLES <charlesmtawaliJr@gmail.com>
2020-01-19 14:19:14 +0300

    configured base and navbar

--- post/views.py
+++ post/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 # Create your views here.
-def index(request):
-    return HttpResponse("hello world")
+# def index(request):
+#     return
